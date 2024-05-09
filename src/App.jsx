6590d1b927b1c6b5a7d0a1b9cdf44f8d5e8dcd81
Leila <leila.kassi@outlook.com>
2024-05-09 15:46:39 -0600

    created modules of the app with a simple function that recalculates amount with no regard to measurement change

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import IngredientsInput from './components/ingredientsInput'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <IngredientsInput />
+    </div>
+  );
+}
+
+export default App;
