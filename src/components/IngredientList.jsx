7d3c53587459a0bebd041ee2c3f2b2585e3e3991
Leila <leila.kassi@outlook.com>
2024-06-19 16:39:03 -0600

    adding editing functionality

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import IngredientInput from "./IngredientInput";
 
-function IngredientList({ingredientData, onEdit}) {
+function IngredientList ( {ingredientData, onEdit, done} ) {
+	let editContent;
+	const [ toEdit, setToEdit ] = useState( false );
+
+	function editHandler ( ingredientName ) {
+		let amountToEdit;
+		ingredientData.forEach((ingredient) => {
+			if( ingredient.ingredient === ingredientName )
+			{
+				amountToEdit = ingredient.amount;
+			}
+		});
+		// const selectedIngredient = ingredientData.find( ( ingredient ) => ingredient.ingredient === ingredientName );
+		// editContent = (
+		// 	<>
+		// 		<IngredientInput
+		// 			type="text"
+		// 			ref={ingredientName} />
+		// 		<IngredientInput
+		// 			type="text"
+		// 			ref={ingredientName} />
+		// 	</>
+		// )
+		// setToEdit( true );
+		onEdit( ingredientName );
+		console.log(amountToEdit)
+	}
+
   return (
 		<section>
-      <li>
-        {ingredientData.map((ingredient) => (
-          <ul key={ingredient.ingredient}>
-            <span>{ingredient.ingredient}</span>
-            <span>{ingredient.amount}</span>
-						<span>{ingredient.measurement}</span>
-						<span><i className="bi bi-pencil-square" onClick={onEdit}></i></span>
-          </ul>
-        ))}
-      </li>
+			{!toEdit ?
+				<li>
+					{ingredientData.map((ingredient) => (
+						<ul key={ingredient.ingredient}>
+							<span>{ingredient.ingredient}</span>
+							<span>{ingredient.amount}</span>
+							<span>{ingredient.measurement}</span>
+							{done ? <span><i className="bi bi-pencil-square" onClick={() => editHandler(ingredient.ingredient)}></i></span> : undefined}
+						</ul>
+					))}
+				</li> :
+				<li>
+					{ingredientData.map((ingredient) => (
+						<ul key={ingredient.ingredient}>
+							<span>{ingredient.ingredient}</span>
+							<span>{ingredient.amount}</span>
+							<span>{ingredient.measurement}</span>
+							<span><i className="bi bi-pencil-square"></i></span>
+						</ul>
+					))}
+				</li>}
+
     </section>
   );
 }
