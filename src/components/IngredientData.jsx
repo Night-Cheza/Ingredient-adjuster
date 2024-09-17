080db3fbc5337a31bf2f83660e151ca7d8ff3ae0
Leila <leila.kassi@outlook.com>
2024-09-17 16:45:15 -0600

    replaced refs with useState for user input

--- src/components/IngredientData.jsx
+++ src/components/IngredientData.jsx
@@ -1,61 +1,87 @@
-import React, {useRef} from "react";
+import React, {useState} from "react";
 import { useContext } from "react";
 import { ListContext } from "../store/recipe-list-context.jsx";
-import IngredientInput from "./IngredientInput.jsx";
 import Button from "./Button.jsx";
+
+const OPTIONS = [
+	"Please select",
+	"Cup (250ml)",
+	"Tablespoon (tbsp)",
+	"Teaspoon (tsp)",
+	"Ounce (oz)",
+	"Liter (L)",
+	"Milliliters (ml)",
+	"Kilogram (kg)",
+	"Gram (g)",
+	"Pound (lb)",
+	"Piece",
+];
 
 function IngredientData () {
 	const {addIngredient} = useContext( ListContext );
-	const ingredientData = useRef();
-	const amount = useRef();
-	const measurement = useRef( "Please select" );
-
+	const [ values, setValues ] = useState( {
+		ingredient: '',
+		amount: '',
+		measurement: ''
+	} );
 
 	//getting user's initial input
+	function inputChangeHandler (inputType, value) {
+		setValues( prevValues => ( {
+			...prevValues,
+			[ inputType ]: value
+		} ) )
+	}
+
 	function submitHandler () {
-		const ingredientInput = ingredientData.current.value.trim().toLowerCase();
-		const amountInput = amount.current.value.trim();
-		const measurementInput = measurement.current.value;
-
-		if (
-			ingredientInput.trim() === "" ||
-			amountInput.trim() === "" ||
-			measurementInput.trim() === "Please select"
-		)
-		{
-			//HANDLE ERROR MESSAGE FOR EMPTY INPUT
-			return;
-		} else if(isNaN(amountInput) || amountInput < 0)
-		{
-			throw Error('Please provide correct amount')
-		}
-
 		addIngredient({
-			ingredient: ingredientInput,
-			amount: amountInput,
-			measurement: measurementInput,
-		});
-
-		ingredientData.current.value = null;
-		amount.current.value = null;
-		measurement.current.value = "Please select";
+			ingredient: values.ingredient,
+			amount: values.amount,
+			measurement: values.measurement,
+		} );
+		//resetting table
+		setValues( {
+			ingredient: '',
+			amount: '',
+			measurement: ''
+		})
 	}
 
 	return (
 		<div className="ingredientInput">
 			<table>
 				<tbody>
-					<IngredientInput
-						type="text"
-						ref={ingredientData}
-						label="Ingredient"
-					/>
-					<IngredientInput
-						type="text"
-						ref={amount}
-						label="Amount"
-					/>
-					<IngredientInput ref={measurement} label="Measurement" select />
+					<tr>
+						<td>
+							<label>Ingredient</label>
+						</td>
+						<td>
+							<input
+								type='text' value={values.ingredient} onChange={( event ) => inputChangeHandler( 'ingredient', event.target.value )} />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label type='text'>Amount</label>
+						</td>
+						<td>
+							<input type='text' name='amount' value={values.amount} onChange={(event) => inputChangeHandler('amount', event.target.value)} />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>Measurement</label>
+						</td>
+						<td>
+							<select name='measurement' value={values.measurement} onChange={(event) => inputChangeHandler('measurement', event.target.value)}>
+								{OPTIONS.map((value, valueIndex) => (
+									<option key={valueIndex} value={value}>
+										{value}
+									</option>
+								))}
+							</select>
+						</td>
+					</tr>
 					<tr>
 						<td colSpan='2'>
 							<Button onClick={submitHandler} text='Add Ingredient' />
