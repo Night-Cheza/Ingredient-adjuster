b60467146271e78ffb3f716429245175924025fd
Leila Nalivkina <leila.kassi@outlook.com>
2025-03-03 14:25:14 -0700

    included fraction 0.99 to conversion

--- src/utils/conversionUtil.jsx
+++ src/utils/conversionUtil.jsx
@@ -7,22 +7,20 @@ export const recalculateRecipe = (ingredients, selectedIngredient, newAmount) =>
 
 	return ingredients.map((ingredient) => {
 		let scaledAmount = ingredient.amount * scaleFactor;
-		// let convertedValues = [];
 
 			// Extract whole and fractional parts
 			const wholePart = Math.floor(scaledAmount);
 			const fractionalPart = scaledAmount - wholePart;
 
 			// Apply conversion only if the fractional part meets the condition
-			const shouldConvert = ((fractionalPart < 0.99 && fractionalPart > 0.50) || (fractionalPart < 0.50 && fractionalPart > 0.00)) && !nonConvertibleUnits.includes(ingredient.unit)
-;
+			const shouldConvert = ((fractionalPart <= 0.99 && fractionalPart > 0.50) || (fractionalPart < 0.50 && fractionalPart > 0.00)) && !nonConvertibleUnits.includes(ingredient.unit);
 
 			let convertedValues = shouldConvert ? convertUnits(fractionalPart, ingredient.unit, ingredient.name) : [];
 
 			return {
 					...ingredient,
 					amount: shouldConvert ? wholePart : parseFloat(scaledAmount.toFixed(2)),
-					convertedValues, // fractional part if no conversion was possible
+					convertedValues,
 			};
 	});
 };
