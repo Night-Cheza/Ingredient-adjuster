9f7ddd56a54e7f4479641732b4543ef8ad9d4e94
Leila Nalivkina <leila.kassi@outlook.com>
2025-03-12 17:47:57 -0600

    fetching data

--- src/utils/fetchIngredientDataUtil.jsx
+++ src/utils/fetchIngredientDataUtil.jsx
@@ -1,73 +1,17 @@
-import axios from "axios";
+import ingredientData from "../data/ingredientList.json"
+import unitData from "../data/units.json"
 
-const API_BASE_URL = "https://food-ingredient-measurement-conversion.p.rapidapi.com";
-
-const HEADERS = {
-	"X-RapidAPI-Key": "fefec5126emshc6f6acc92fec029p1d9781jsndb00639f65c9",
-	"X-RapidAPI-Host": "food-ingredient-measurement-conversion.p.rapidapi.com",
+export const fetchCategoryList = () => {
+	const category = ingredientData.map(item => item.category)
+	return category;
 };
 
-// Fetch list of ingredients
-export const fetchIngredientList = async () => {
-	try {
-		const response = await axios.get( `${API_BASE_URL}/list-ingredients`, {headers: HEADERS} );
-		const ingredientsList = response.data[ 0 ].details;
-		// console.log( 'Ingredients: ', `${response.data[ 0 ].details}` );
-		// If no response data returned
-		if (!response.data[0].details) {
-			return [];
-		}
-
-		// Group ingredients by name
-		const groupedByName = ingredientsList.reduce((listByName, item) => {
-			if (!listByName[item.ingredient]) {
-				listByName[item.ingredient] = [];
-			}
-			listByName[item.ingredient].push(item);
-			return listByName;
-		}, {});
-
-		// Filter on brand "generic"
-		const filteredIngredients = Object.values(groupedByName).map(ingredientGroup => {
-			const brand = ingredientGroup.find(ingredient => ingredient.brand === "generic");
-			return brand || ingredientGroup[0]; // If no generic brand, return the first occurrence
-		});
-
-		return filteredIngredients;
-	} catch (error) {
-		console.error("Error fetching ingredient list:", error);
-		return [];
-	}
+export const fetchIngredientByCategory = (category) => {
+	const categoryData = ingredientData.find( item => item.category === category );
+	return categoryData ? categoryData.ingredients : [];
 };
 
-// Fetch list of measurement units
-export const fetchUnitList = async () => {
-	try {
-		const response = await axios.get( `${API_BASE_URL}/list-units`, {headers: HEADERS} );
-		const resp = [...response.data[ 0 ].details, ...response.data[ 1 ].details]
-		console.log( 'Measurements: ', `${response.data[ 0 ].details}` );
-		if (!response.data[0].details || !response.data[ 1 ].details) {
-			return [];
-		}
-		return resp || []; // Return list of measurement units
-	} catch (error) {
-			console.error("Error fetching unit list:", error);
-			return [];
-	}
-};
-
-
-// Convert measurement using API
-export const fetchMeasurementConversion = async (name, amount, fromUnit, toUnit) => {
-	try {
-		const response = await axios.get(`${API_BASE_URL}/convert`, {
-			params: { ingredient: name, from: fromUnit, to: toUnit, value: amount },
-			headers: HEADERS,
-		} );
-		console.log( 'Converted: ', `${response.data}` );
-		return response.data; // Return converted values
-	} catch (error) {
-			console.error("Error fetching measurement conversion:", error);
-			return null;
-	}
-};
+export const fetchUnitList = () => {
+	const unit = unitData.map(item => item.unit)
+	return unit;
+}
