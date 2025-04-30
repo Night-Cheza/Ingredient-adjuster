8a4a6eaf69efe75f19e9660c42af91b2e775f219
Leila Nalivkina <leila.kassi@outlook.com>
2025-04-30 14:40:10 -0600

    displaying converted and recalculated ingredients

--- src/components/RecalculatedIngredients.jsx
+++ src/components/RecalculatedIngredients.jsx
@@ -1,18 +1,35 @@
-const RecalculatedIngredients = ( {recalculated, onReset} ) => {
-	// const handleConvert = () => { };
 
+
+const RecalculatedIngredients = ( {recalculated, onReset, convertedRecipe, onConvert, isConverted} ) => {
 	return (
 		<div>
-			<h2>Adjusted Recipe</h2>
-			<ul>
-				{recalculated.map((ingredient, index) => (
-					<li key={index}>
-						{ingredient.name}: {ingredient.amount} {ingredient.unit}
-						{/* <button onClick={handleConvert}>Convert</button> */}
-					</li>
-				) )}
-				<button className ="resetbtn" onClick={onReset}>Reset</button>
-			</ul>
+			{!isConverted ?
+				<>
+					<h2>Adjusted Recipe</h2>
+					<ul>
+						{recalculated.map((ingredient, index) => (
+							<li key={index}>
+								{ingredient.name}: {ingredient.amount} {ingredient.unit}
+							</li>
+						) )}
+						<button onClick={onConvert}>Convert</button>
+						<button onClick={onReset}>Reset</button>
+					</ul>
+				</>
+				:
+				<>
+					<h2>Converted recipe Recipe</h2>
+					<ul>
+						{convertedRecipe.map((ingredient, index) => (
+							<li key={index}>
+								{ingredient.name}: {ingredient.amount} {ingredient.unit}
+							</li>
+						) )}
+						<button onClick={onReset}>Reset</button>
+					</ul>
+				</>
+			}
+
 		</div>
 	);
 };
